9cb22c7d9a1b12a89644231d9e33b943c9f3ea97
valkovik15 <kopotchel@outlook.com>
2019-03-29 16:27:44 +0300

    lvl5__

--- config/environment.rb
+++ config/environment.rb
@@ -4,15 +4,3 @@ require 'net/http'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-logger = Logger.new logfile.log
-Logger.level = Logger::WARN
-
-# logs for program called MainProgram
-logger.progname = 'MainProgram'
-
-# a simple formatter
-logger.formatter = proc do |severity, time, progname, msg|
-  "#{datetime}: #{msg} from #{progname} \n"
-end
-
-logger.info("Some error.")
